3a99eed12dc959355bc49eac0f66532a1f2085b0
cissica <cecemartinez@hotmail.com>
2021-05-06 18:14:08 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('form');
+
+  form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    let inputTag = document.querySelector('input#new-task-description');
+    let inputValue = inputTag.value;
+
+
+    let newLi = document.createElement('li');
+    newLi.innerText = inputValue;
+
+    let ulTag = document.querySelector('ul#tasks');
+    ulTag.append(newLi);
+    inputTag.value = '';
+    
+    const button = document.createElement("button");
+    button.innerHTML = 'X'
+    newLi.appendChild(button);
+  
+  })
 });
+
